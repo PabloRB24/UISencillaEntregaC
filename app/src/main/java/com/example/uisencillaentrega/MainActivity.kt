09db4fc70b72930ae085a24ad96a9b841ebde7c4
PabloRB24 <pabrod138@alu.centroafuera.es>
2023-10-31 21:25:40 +0100

    Ui simple commit

--- app/src/main/java/com/example/uisencillaentrega/MainActivity.kt
+++ app/src/main/java/com/example/uisencillaentrega/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.uisencillaentrega
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            counter++
+            val message = "Se ha hecho clic en el botón $counter veces"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
+
+
